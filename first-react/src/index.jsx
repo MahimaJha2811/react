9c879a9bd1102046f703824f5b1570670793c783
MahimaJha2811 <mahimajha0811@gmail.com>
2024-12-08 20:04:37 +0530

    challenge-2
    1) commenting before challenge code
    2)challenge-2 starts
    3)created a new function called page
    4) learn about how to use header,footer and main tag
    5) learnt about how to render function in root.render and one more thing function name always starts with capital letter than follows camelcase pattern

--- first-react/src/index.jsx
+++ first-react/src/index.jsx
@@ -1,20 +1,47 @@
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
-root.render(temporaryReact());
 
-function temporaryReact() {
+// function temporaryReact() {
+//   return (
+//     <main>
+//       <img src="src/reactLogo.png" />
+//       <h1>"Fun facts about React!"</h1>
+//       <ul>
+//         <li>"Was first released in 2013"</li>
+//         <li>"Was originally created by Walke"</li>
+//         <li>"Has well over 100k stars on GitHub"</li>
+//         <li>"Is maintained by Meta"</li>
+//         <li>"Powers thousands of enterprise apps, including mobile apps"</li>
+//       </ul>
+//     </main>
+//   );
+// }
+
+function Page() {
   return (
-    <main>
-      <img src="src/reactLogo.png" />
-      <h1>"Fun facts about React!"</h1>
-      <ul>
-        <li>"Was first released in 2013"</li>
-        <li>"Was originally created by Walke"</li>
-        <li>"Has well over 100k stars on GitHub"</li>
-        <li>"Is maintained by Meta"</li>
-        <li>"Powers thousands of enterprise apps, including mobile apps"</li>
-      </ul>
-    </main>
+    <div>
+      <header>
+        <img src="src/reactLogo.png" width="30px" />{" "}
+      </header>
+      <main>
+        <h1>"Reasons I'm excited to learn React"</h1>
+        <ol>
+          <li>
+            React is a popular library, so I will be able to fit in with all the
+            coolest devs out there! 😎
+          </li>
+          <li>
+            I am more likely to get a job as a front end developer if I know
+            React
+          </li>
+        </ol>{" "}
+      </main>
+      <footer>
+        <small>© 2024 Jha development. All rights reserved</small>
+      </footer>
+    </div>
   );
 }
+
+root.render(<Page />);
