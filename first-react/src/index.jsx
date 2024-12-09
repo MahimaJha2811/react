faf2cc44e9fe3c066211f7bed5af2927cad831ed
MahimaJha2811 <mahimajha0811@gmail.com>
2024-12-09 22:12:29 +0530

    parent/child components
    Learning about how we can make so many custom components and use them as parent and child like making a hierarchy tree

--- first-react/src/index.jsx
+++ first-react/src/index.jsx
@@ -19,28 +19,44 @@ const root = createRoot(document.getElementById("root"));
 //   );
 // }
 
+function Header() {
+  return (
+    <header>
+      <img src="src/reactLogo.png" width="30px" />{" "}
+    </header>
+  );
+}
+
+function MainContent() {
+  return (
+    <main>
+      <h1>"Reasons I'm excited to learn React"</h1>
+      <ol>
+        <li>
+          React is a popular library, so I will be able to fit in with all the
+          coolest devs out there! 😎
+        </li>
+        <li>
+          I am more likely to get a job as a front end developer if I know React
+        </li>
+      </ol>{" "}
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2024 Jha development. All rights reserved</small>
+    </footer>
+  );
+}
 function Page() {
   return (
     <>
-      <header>
-        <img src="src/reactLogo.png" width="30px" />{" "}
-      </header>
-      <main>
-        <h1>"Reasons I'm excited to learn React"</h1>
-        <ol>
-          <li>
-            React is a popular library, so I will be able to fit in with all the
-            coolest devs out there! 😎
-          </li>
-          <li>
-            I am more likely to get a job as a front end developer if I know
-            React
-          </li>
-        </ol>{" "}
-      </main>
-      <footer>
-        <small>© 2024 Jha development. All rights reserved</small>
-      </footer>
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   );
 }
