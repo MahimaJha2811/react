24ec7b785063455cecbfe9c6ba24c70713df2437
MahimaJha2811 <mahimajha0811@gmail.com>
2024-12-11 01:19:59 +0530

    Styling of AppMainContent.jsx file

--- first-react/src/assets/AppComponents/AppMainContent.jsx
+++ first-react/src/assets/AppComponents/AppMainContent.jsx
@@ -1,16 +1,18 @@
 export default function AppMainContent() {
   return (
     <>
-      <h1>"Fun facts about React!"</h1>
-      <ul>
-        <li className="list-item">Was first released in 2013</li>
-        <li className="list-item">Was originally created by Walke</li>
-        <li className="list-item">Has well over 100k stars on GitHub</li>
-        <li className="list-item">Is maintained by Meta</li>
-        <li className="list-item">
-          Powers thousands of enterprise apps, including mobile apps
-        </li>
-      </ul>
+      <main>
+        <h1 className="AppMainH1">"Fun facts about React!"</h1>
+        <ul className="AppList">
+          <li className="list-item">Was first released in 2013</li>
+          <li className="list-item">Was originally created by Walke</li>
+          <li className="list-item">Has well over 100k stars on GitHub</li>
+          <li className="list-item">Is maintained by Meta</li>
+          <li className="list-item">
+            Powers thousands of enterprise apps, including mobile apps
+          </li>
+        </ul>
+      </main>
     </>
   );
 }
