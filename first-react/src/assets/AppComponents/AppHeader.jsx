effb221f542071cd5eaffb0a7906427cc29d0978
MahimaJha2811 <mahimajha0811@gmail.com>
2024-12-11 00:57:17 +0530

    styling of AppHeader.jsx

--- first-react/src/assets/AppComponents/AppHeader.jsx
+++ first-react/src/assets/AppComponents/AppHeader.jsx
@@ -1,8 +1,12 @@
 export default function AppHeader() {
   return (
     <>
-      <img src="reactLogo.png" className="appImg" alt="ReactLogo"></img>
-      <span className="heading">ReactFacts</span>
+      <header>
+        <nav>
+          <img src="reactLogo.png" className="appImg" alt="ReactLogo"></img>
+          <span className="heading">ReactFacts</span>
+        </nav>
+      </header>
     </>
   );
 }
